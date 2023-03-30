d9a916ccbd03fb78a3f974f5644062efced8a548
lewiskk016 <lewiskk016@gmail.com>
2023-03-30 14:44:48 -0400

    first_anagram

--- problem1.rb
+++ problem1.rb
@@ -23,31 +23,31 @@
 # p my_min(list)  # =>  -5
 
 
-def largest_contiguous_subsum(list) 
+def largest_contiguous_subsum(list)
 
         current_count = 0
         current_max = list[0]
-        list.each do |ele| 
-            current_count += ele  
-            current_max = current_count if current_count > current_max 
-            current_count = 0 if current_count < 0 
+        list.each do |ele|
+            current_count += ele
+            current_max = current_count if current_count > current_max
+            current_count = 0 if current_count < 0
         end
-        current_max 
-end 
+        current_max
+end
 
 # def hello(list)
 #     current_sum = 0
 #     current_max = 0
 
 #     list.each do |num|
-#         if num < 0 
-#             current_max = current_sum 
-#         end 
-#         if current_sum > current_max 
-#             current_max = current_sum 
+#         if num < 0
+#             current_max = current_sum
+#         end
+#         if current_sum > current_max
+#             current_max = current_sum
 #         end
 #     end
-#     current_max 
+#     current_max
 
 # end
 
@@ -66,19 +66,34 @@ end
 
 
 
-list = [5, 3, -7]
-p largest_contiguous_subsum(list) # => 8
+# list = [5, 3, -7]
+# p largest_contiguous_subsum(list) # => 8
 
-# # possible sub-sums
-# [5]           # => 5
-# [5, 3]        # => 8 --> we want this one
-# [5, 3, -7]    # => 1
-# [3]           # => 3
-# [3, -7]       # => -4
-# [-7]          # => -7
+# # # possible sub-sums
+# # [5]           # => 5
+# # [5, 3]        # => 8 --> we want this one
+# # [5, 3, -7]    # => 1
+# # [3]           # => 3
+# # [3, -7]       # => -4
+# # [-7]          # => -7
 
-list = [2, 3, -6, 7, -6, 7]
-p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+# list = [2, 3, -6, 7, -6, 7]
+# p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
 
-list = [-5, -1, -3]
-p largest_contiguous_subsum(list) # => -1 (from [-1])
+# list = [-5, -1, -3]
+# p largest_contiguous_subsum(list) # => -1 (from [-1])
+
+def first_anagram?(word1, word2)
+
+    #
+    word1.chars.permutation.to_a.include?(word2)
+
+    p word1.chars.permutation.to_a
+
+end
+
+
+
+
+p first_anagram?("gizmo", "sally")    #=> false
+p first_anagram?("elvis", "lives")    #=> true
