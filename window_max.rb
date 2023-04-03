4d5ba36e212a3f5a7792e11b2521b0d8260cbb1f
vinicozzi <vinicozzimincent@gmail.com>
2023-04-02 22:20:22 -0400

    changes

--- window_max.rb
+++ window_max.rb
@@ -8,11 +8,23 @@ def windowed_max_range(array, window_size)
 
     vals = subs.select { |sub| sub.length  == window_size - array.length }
 end 
-  Generate all possible windows of the given size
-  
-  
+ 
+p windowed_max_range([1, 0, 2, 5, 4, 8], 2) #== 4 # 4, 8
+p windowed_max_range([1, 0, 2, 5, 4, 8], 3) #== 5 # 0, 2, 5
+p windowed_max_range([1, 0, 2, 5, 4, 8], 4) #== 6 # 2, 5, 4, 8
+p windowed_max_range([1, 3, 2, 5, 4, 8], 5) #== 6 # 3, 2, 5, 4, 8
 
-p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
-p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
-p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
-p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
+def two_sum?(arr, target_sum)
+  complements = {}
+
+  arr.each do |el|
+    return true if complements[target_sum - el]
+    complements[el] = true
+  end
+
+  false
+end
+
+arr = [0, 1, 5, 7]
+p two_sum?(arr, 6) # => should be true
+p two_sum?(arr, 10) # => should be false
